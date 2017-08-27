053f293fe8ffa866b37ae7b1f757bd7c78a77f1c
German Rincon Urrego <grinconu@MacBook-Pro-de-German.local>
2017-08-27 15:10:23 -0500

    CAMBIOS DE SERVICIOS

--- Services/ApiRest/Routes/Persona.js
+++ Services/ApiRest/Routes/Persona.js
@@ -9,4 +9,13 @@ api.put('/personas',personaController.updatePersonas);
 api.get('/personas/:id',personaController.findById);
 api.delete('/personas/:id',personaController.deleteById);
 
+api.use((req,res,next) => {
+    res.header('Access-Control-Allow-Origin','*');
+    res.header('Access-Control-Allow-Headers','X-API-KEY,Origin, X-Requested-with, Content-Type,Accept,Access-Control-Request-Method');
+    res.header('Access-Control-Allow-Methods','GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow','GET, POST, OPTIONS, PUT, DELETE')
+    
+    next();
+});
+
 module.exports = api;
